df299d9c48be5ce2e16fc180f5b8f4697ea8abb7
Nirosh76 <niroshlk@gmail.com>
2025-01-01 18:23:04 +0530

    Shadcn Card

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,4 +1,14 @@
 import { getMovies } from "@/app/libs/apis/server";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Image from "next/image";
 import React from "react";
 
 export default async function dashboardpage() {
@@ -25,8 +35,28 @@ export default async function dashboardpage() {
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
           {moviesQuery?.length &&
             moviesQuery.map((movie) => (
-              <div key={movie._id} className="h-96 bg-cyan-500 ">
-                {movie.title}{" "}
+              <div key={movie._id} className="h-96">
+                <Card className="h-full">
+                  <CardHeader>
+                    <CardTitle>{movie?.title}</CardTitle>
+                    <CardDescription className="sr-only">
+                      {movie?.plot}
+                    </CardDescription>
+                  </CardHeader>
+                  <CardContent>
+                    <div className="flex justify-center bg-black w-full h-[276px] mb-4 rounded">
+                      <Image
+                        src={movie?.poster}
+                        alt={movie?.title}
+                        width={200}
+                        height={400}
+                      />
+                    </div>
+                  </CardContent>
+                  <CardFooter>
+                    <p>Card Footer</p>
+                  </CardFooter>
+                </Card>
               </div>
             ))}
         </div>
